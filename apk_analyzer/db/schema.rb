6113826285cd8ce0e2de7268b3e6aea53f92096e
Daniel <github@zwischenablage.org>
2020-11-13 00:23:07 +0100

    initial commit for datatables based on BS4
    https://aarvy.me/blog/2019/09/21/datatables-with-bootstrap-4-minimal-setup-in-rails-6/

---
+++ apk_analyzer/db/schema.rb
@@ -0,0 +1,32 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_11_12_221233) do
+
+  create_table "apps", force: :cascade do |t|
+    t.string "filename"
+    t.string "package"
+    t.boolean "hasActivity"
+    t.boolean "hasRO"
+    t.integer "numOfServices"
+    t.integer "numOfReceivers"
+    t.integer "numOfProviders"
+    t.integer "createdPermissions"
+    t.integer "usedPermissions"
+    t.integer "bootCompleted"
+    t.integer "persistent"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.boolean "hasApplication"
+  end
+
+end
